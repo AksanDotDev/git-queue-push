3968061ba0b5a842d2732d64545d871a0488b12f
AC-Aksan <seraphofthemaelstrom@googlemail.com>
2020-01-25 16:52:36 +0000

    Updated for SSH access

--- git-queue-push.sh
+++ git-queue-push.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-# Get Origin URL
+# Get Origin URL and validate
 TARGETRAW=$(git remote get-url origin)
-TARGET=${TARGETRAW%%".git"}
+if [ -z "$TARGETRAW" ]
+then
+    echo "error: remote not found"
+    exit 1
+fi
+# echo $TARGETRAW
+MARK1=$(expr index "$TARGETRAW" '@')
+if [ $MARK1 -eq 0 ]
+then
+    echo "error: ssh remote not found, update origin"
+    exit 1
+fi
+# echo $MARK1
+MARK2=$(expr index "$TARGETRAW" ':')
+# echo $MARK2
+TARGET=${TARGETRAW:$MARK1:$MARK2-$MARK1-1}
 
-
+# echo $TARGET
 
 while :
 do 
@@ -12,6 +27,7 @@ do
     wget -q --spider $TARGET
 
     if [ $? -eq 0 ]; then
+        echo "go"
         git push
         break
     fi
